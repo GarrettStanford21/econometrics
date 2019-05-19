0cae571e731589a90b639f463c36b48b2d9a992c
rcberg <donbarno@gmail.com>
2019-05-19 11:26:44 -0700

    might need to change more later

--- ps01.R
+++ ps01.R
@@ -7,8 +7,8 @@ p_load(tidyverse ,
        estimatr)
 
 ## HW Number 1 
-nsw_df <- read_dta("D:/Economics/Class Materials, Assignments, Papers/UO/Econometrics III (Econ. 525)/nsw.dta")
-psid_df <- read_dta("D:/Economics/Class Materials, Assignments, Papers/UO/Econometrics III (Econ. 525)/psid_controls.dta")
+nsw_df <- read_dta("http://users.nber.org/~rdehejia/data/nsw.dta")
+psid_df <- read_dta("http://users.nber.org/~rdehejia/data/psid_controls.dta")
 
 ## HW Number 2
 
@@ -64,12 +64,11 @@ covariates_ntreat <-  mixed_tbl %>% filter(treat==0) %>% select(age , education 
 
 ## HW Number 13
 model_n13 <- treat ~ age + education + black + hispanic + married + nodegree # Six covariates Ed mentions in 11 that I assume he wants us using
-logit <- glm(data = mixed_tbl , formula = model_n13 , family = binomial) # Logit regression for propensity scores
+logit <- glm(data = mixed_tbl , formula = model_n13 , family = binomial()) # Logit regression for propensity scores
 pro_scores <- enframe(logit$fitted.values)  %>% bind_cols(mixed_tbl) %>% rename(p.score = value) # Propensity scores
 
 ps_treat <- pro_scores %>% filter( treat==1 ) # Want to get the lowest propensity score that any of the treated have
 min_ps_treat <- min( ps_treat$p.score )
-overlap_treat <- pro_scores %>% filter( p.score > min_ps_treat & treat==0 ) # Only look at untreated with at least that p-score
 ps_untreat <- pro_scores %>% filter( treat==0 ) # Similarly get the highest propensity score that a nontreated has
 max_ps_untreat <- max( ps_untreat$p.score )
 
@@ -77,10 +76,10 @@ max_ps_untreat <- max( ps_untreat$p.score )
 ## IMPORTANT! If you want to mess with enforcing overlap using P scores as Ed did, this next line is where to adjust!
 ## 
 
-new_df <- pro_scores %>% filter( p.score > 0.05 & p.score < 0.45 ) 
+new_df <- pro_scores %>% filter( p.score >= min_ps_treat & treat==0 | p.score <= max_ps_untreat & treat==1 ) 
 
 ggplot(data = new_df , aes( x = p.score , fill = as.factor(treat) , color = as.factor(treat) )) +
-  geom_histogram( binwidth = 0.05 , alpha = 0.6)
+  geom_histogram( binwidth = 0.01 , alpha = 0.6)
 
 lm_robust(data = new_df , 
           formula = re78 ~ p.score + treat) %>% tidy
@@ -91,32 +90,32 @@ lm_robust(data = new_df ,
 ## If you choose your own threshold for p scores to enforce overlap, use sequence below to easily create blocks!!
 ## 
 
-block_min <- 0.05
-block_max <- 0.45
+block_min <- min_ps_treat
+block_max <- max_ps_untreat
 block_size <- (block_max - block_min) / 20
 block_bins <- seq(from = block_min , to = block_max , by = block_size)
 
 new_df <- new_df %>% mutate( block = case_when(
-                               p.score > block_bins[1]  & p.score<= block_bins[2]  ~ 1  ,
-                               p.score > block_bins[2]  & p.score<= block_bins[3]  ~ 2  , 
-                               p.score > block_bins[3]  & p.score<= block_bins[4]  ~ 3  , 
-                               p.score > block_bins[4]  & p.score<= block_bins[5]  ~ 4  , 
-                               p.score > block_bins[5]  & p.score<= block_bins[6]  ~ 5  ,
-                               p.score > block_bins[6]  & p.score<= block_bins[7]  ~ 6  ,
-                               p.score > block_bins[7]  & p.score<= block_bins[8]  ~ 7  ,
-                               p.score > block_bins[8]  & p.score<= block_bins[9]  ~ 8  ,
-                               p.score > block_bins[9]  & p.score<= block_bins[10] ~ 9  ,
-                               p.score > block_bins[10] & p.score<= block_bins[11] ~ 10 ,
-                               p.score > block_bins[11] & p.score<= block_bins[12] ~ 11 ,
-                               p.score > block_bins[12] & p.score<= block_bins[13] ~ 12 ,
-                               p.score > block_bins[13] & p.score<= block_bins[14] ~ 13 ,
-                               p.score > block_bins[14] & p.score<= block_bins[15] ~ 14 ,
-                               p.score > block_bins[15] & p.score<= block_bins[16] ~ 15 ,
-                               p.score > block_bins[16] & p.score<= block_bins[17] ~ 16 ,
-                               p.score > block_bins[17] & p.score<= block_bins[18] ~ 17 ,
-                               p.score > block_bins[18] & p.score<= block_bins[19] ~ 18 , 
-                               p.score > block_bins[19] & p.score<= block_bins[20] ~ 19 ,
-                               p.score > block_bins[20] & p.score<= block_bins[21] ~ 20
+                               p.score >= block_bins[1]  & p.score<= block_bins[2]  ~ 1  ,
+                               p.score >  block_bins[2]  & p.score<= block_bins[3]  ~ 2  , 
+                               p.score >  block_bins[3]  & p.score<= block_bins[4]  ~ 3  , 
+                               p.score >  block_bins[4]  & p.score<= block_bins[5]  ~ 4  , 
+                               p.score >  block_bins[5]  & p.score<= block_bins[6]  ~ 5  ,
+                               p.score >  block_bins[6]  & p.score<= block_bins[7]  ~ 6  ,
+                               p.score >  block_bins[7]  & p.score<= block_bins[8]  ~ 7  ,
+                               p.score >  block_bins[8]  & p.score<= block_bins[9]  ~ 8  ,
+                               p.score >  block_bins[9]  & p.score<= block_bins[10] ~ 9  ,
+                               p.score >  block_bins[10] & p.score<= block_bins[11] ~ 10 ,
+                               p.score >  block_bins[11] & p.score<= block_bins[12] ~ 11 ,
+                               p.score >  block_bins[12] & p.score<= block_bins[13] ~ 12 ,
+                               p.score >  block_bins[13] & p.score<= block_bins[14] ~ 13 ,
+                               p.score >  block_bins[14] & p.score<= block_bins[15] ~ 14 ,
+                               p.score >  block_bins[15] & p.score<= block_bins[16] ~ 15 ,
+                               p.score >  block_bins[16] & p.score<= block_bins[17] ~ 16 ,
+                               p.score >  block_bins[17] & p.score<= block_bins[18] ~ 17 ,
+                               p.score >  block_bins[18] & p.score<= block_bins[19] ~ 18 , 
+                               p.score >  block_bins[19] & p.score<= block_bins[20] ~ 19 ,
+                               p.score >  block_bins[20] & p.score<= block_bins[21] ~ 20
   
                              ) 
                             ) # Generates 20 equal size blocks based on block_min & block_max & step size.
@@ -142,23 +141,23 @@ tau
 
 # Double robustness
 
-model_n13f <- re78 ~ age + education + black + hispanic + married + nodegree + treat
+model_n13f <- re78 ~ age + education + black + hispanic + married + nodegree + treat # "Six covariates" Ed refers us to use
 double_robust_coefs <- c(rep(0,20))
 
 for( i in 1:20 ) {
   
 block_i <- new_df %>% filter( block == i )
 reg_block_i <- lm_robust( data = block_i , formula = model_n13f )
-d_robust_coefs[i] <- coef(reg_block_i)[8]
-double_robust_coefs[i] <- d_robust_coefs[[i]]
+double_robust_coefs[i] <- coef(reg_block_i)[8]
+double_robust_coefs[i] <- double_robust_coefs[[i]]
 
-}
+} # Loop filters each block and runs the regression we'd like on it, then puts the treatment coefficient in a vector
 
 t_k <- enframe(double_robust_coefs) %>% rename(block = name , tau_k = value) %>% bind_cols(block_n) %>% 
-  mutate(weighted_effect = tau_k*block_wgt) %>% select(-c(block1))
+  mutate(weighted_effect = tau_k*block_wgt) %>% select(-c(block1)) # Includes vector of block effects & weights
 
 t_block <- t_k %>% summarise(tau = sum(weighted_effect)) %>% as.numeric
-t_block
+t_block # Doubly robust estimate
 
 ## HW Number 14
 
@@ -231,7 +230,7 @@ sim_df_v2 <- bind_rows(sim_list_v2) %>% rename(bias = x)
 var_4_graph <- ggplot(data = sim_df_v4 , aes( x = estimate , fill = as.factor(bias) , color = as.factor(bias))) + 
   geom_density( stat = 'density' , alpha = 0.6 ) +
   geom_vline(xintercept = 5 , color = 'red' , size = 1) + 
-  ggtitle("Omitting Ability from Wage Equation") +
+  ggtitle("Omitting Ability from Wage Equation (Blue is Biased Model)") +
   scale_x_continuous(name = 'Estimate of Education on Wage') +
   scale_y_continuous(name = 'Density') + 
   theme( legend.title = element_blank()) 
@@ -239,7 +238,7 @@ var_4_graph <- ggplot(data = sim_df_v4 , aes( x = estimate , fill = as.factor(bi
 var_6_graph <- ggplot(data = sim_df_v6 , aes( x = estimate , fill = as.factor(bias) , color = as.factor(bias))) + 
   geom_density( stat = 'density' , alpha = 0.6 ) +
   geom_vline(xintercept = 5 , color = 'red' , size = 1) + 
-  ggtitle("Omitting Ability from Wage Equation") +
+  ggtitle("Omitting Ability from Wage Equation (Blue is Biased Model)") +
   scale_x_continuous(name = 'Estimate of Education on Wage') +
   scale_y_continuous(name = 'Density') + 
   theme( legend.title = element_blank()) 
@@ -248,7 +247,7 @@ var_6_graph <- ggplot(data = sim_df_v6 , aes( x = estimate , fill = as.factor(bi
 var_2_graph <- ggplot(data = sim_df_v2 , aes( x = estimate , fill = as.factor(bias) , color = as.factor(bias))) + 
   geom_density( stat = 'density' , alpha = 0.6 ) +
   geom_vline(xintercept = 5 , color = 'red' , size = 1) + 
-  ggtitle("Omitting Ability from Wage Equation") +
+  ggtitle("Omitting Ability from Wage Equation (Blue is Biased Model)") +
   scale_x_continuous(name = 'Estimate of Education on Wage') +
   scale_y_continuous(name = 'Density') + 
   theme( legend.title = element_blank()) 
