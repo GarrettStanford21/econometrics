42938b7a442307f6bfea518c4204d2cc01a2424f
rcberg <donbarno@gmail.com>
2019-05-28 18:39:34 -0700

    fixed some real rookie shit

--- ps02.R
+++ ps02.R
@@ -34,9 +34,6 @@ u_vcov  <- c( 0 , 0 , 3 , 0 ) # " " for u
 nu_vcov <- c( 0 , 0 , 0 , 7 ) # " " for nu
 
 vcov <- matrix( c(x_vcov , e_vcov , u_vcov , nu_vcov ) , ncol = 4 ) # combining into variance covariance matrix
-sys_df <- mvrnorm( n = N , mu = mu_vec , Sigma = vcov ) # building linear system from vcov
-colnames(sys_df) <- c("x" , "e" , "u" , "nu") #this is the right way to name columns in a matrix
-sys_df <- sys_df %>% as_tibble # now I can make the matrix into a tibble
 
 # models for the problem
 w1_model <- y ~ w1
@@ -47,27 +44,33 @@ multi_model <- y ~ w1 + w2
 
 it_count <- 1000
 sim_function <- function( i ){
+  sys_df <- mvrnorm( n = N , mu = mu_vec , Sigma = vcov ) # building linear system from vcov
+  colnames(sys_df) <- c("x" , "e" , "u" , "nu") #this is the right way to name columns in a matrix
+  sys_df <- sys_df %>% as_tibble # now I can make the matrix into a tibble
+  
   sim_model_i = tibble( x = sys_df$x ,
                         u = sys_df$u ,
                         e = sys_df$e , 
                         nu = sys_df$nu , 
                         w1 = x + e , 
                         w2 = x + nu  ,
-                        y = 3 + 7*x + u )
+                        y = 3 + 7*x + u ) # specifying model DGP
 
-w1_reg <-  lm_robust( data = sim_model_i , formula = w1_model ) %>% tidy %>% select(1:4) %>% 
-  filter(term = "w1") %>% bind_cols(model = rep( 1 , 2 ))
-w2_reg <-  lm_robust( data = sim_model_i , formula = w2_model ) %>% tidy %>% select(1:4) %>% 
-  filter(term = "w1") %>% bind_cols(model = rep( 2 , 2 ))
-multi_reg <-  lm_robust( data = sim_model_i , formula = multi_model ) %>% tidy %>% select(1:4) %>% 
-  filter(term = "w1") %>% bind_cols(model = rep( 3 , 3 ))
-w1_iv <-  iv_robust( data = sim_model_i , formula = w1_iv_model ) %>% tidy %>% select(1:4) %>% 
-  filter(term = "w1") %>% bind_cols(model = rep( 4 , 2 ))
-w2_iv <-  iv_robust( data = sim_model_i , formula = w2_iv_model ) %>% tidy %>% select(1:4) %>% 
-  filter(term = "w1") %>% bind_cols(model = rep( 5 , 2 ))
+w1_reg <-  lm_robust( data = sim_model_i , formula = w1_model ) %>% tidy %>% dplyr::select(1:4) %>% 
+  filter(term == "w1") %>% bind_cols(model = rep( 1 , 1 ))
+w2_reg <-  lm_robust( data = sim_model_i , formula = w2_model ) %>% tidy %>% dplyr::select(1:4) %>% 
+  filter(term == "w2") %>% bind_cols(model = rep( 2 , 1 ))
+multi_reg <-  lm_robust( data = sim_model_i , formula = multi_model ) %>% tidy %>% dplyr::select(1:4) %>% 
+  filter(term == "w1" | term== "w2") %>% bind_cols(model = rep( 3 , 2 ))
+w1_iv <-  iv_robust( data = sim_model_i , formula = w1_iv_model ) %>% tidy %>% dplyr::select(1:4) %>% 
+  filter(term == "w1") %>% bind_cols(model = rep( 4 , 1 ))
+w2_iv <-  iv_robust( data = sim_model_i , formula = w2_iv_model ) %>% tidy %>% dplyr::select(1:4) %>% 
+  filter(term == "w2") %>% bind_cols(model = rep( 5 , 1 ))
   
+bind_rows( w1_reg , w2_reg , multi_reg , w1_iv , w2_iv )
 }
 
+set.seed(414)
 sim_df <- map_dfr(1:it_count , sim_function) %>% arrange(model)
 
 
